d6ffa21a2022a86f434d15855380a42aae95aadb
si3nloong <sianloong90@gmail.com>
2020-11-26 03:22:47 +0800

    📦 chore: add development files

---
+++ public/Component.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let onChange = (_: any) => {};
+  export let onBlur = () => {};
+</script>
+
+<style>
+  /* your styles go here */
+</style>
+
+<input type="text" on:input={onChange} on:blur={onBlur} />
