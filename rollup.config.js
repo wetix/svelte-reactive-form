a43b81241176436c17399aaa37be66326463ff60
si3nloong <sianloong90@gmail.com>
2020-12-10 12:08:18 +0800

    fix: reactive-form

--- rollup.config.js
+++ rollup.config.js
@@ -42,40 +42,4 @@ export default [
       // terser(),
     ],
   },
-  {
-    input: "src/rules/index.ts",
-    output: [
-      { file: "rules/index.cjs", format: "cjs" },
-      { file: "rules/index.js", format: "es" },
-    ],
-    plugins: [
-      svelte({
-        preprocess: sveltePreprocess(),
-        compilerOptions: {
-          // enable run-time checks when not in production
-          dev: false,
-        },
-      }),
-
-      // If you have external dependencies installed from
-      // npm, you'll most likely need these plugins. In
-      // some cases you'll need additional configuration -
-      // consult the documentation for details:
-      // https://github.com/rollup/plugins/tree/master/packages/commonjs
-      resolve({
-        browser: true,
-        dedupe: ["svelte"],
-      }),
-      commonjs(),
-      typescript({
-        sourceMap: false,
-      }),
-
-      size(),
-
-      // If we're building for production (npm run build
-      // instead of npm run dev), minify
-      // terser(),
-    ],
-  },
 ];
