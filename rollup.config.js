9aa88c2651e18aaea56e5be73489724ba74313b4
Si3nLoong <sianloong90@gmail.com>
2021-01-24 17:35:52 +0800

    chore: remove unused pkgs

--- rollup.config.js
+++ rollup.config.js
@@ -1,72 +1,72 @@
-import fs from "fs";
-import glob from "glob";
 import svelte from "rollup-plugin-svelte";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import sveltePreprocess from "svelte-preprocess";
+import { terser } from "rollup-plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import size from "rollup-plugin-bundle-size";
 
-const root = "packages/";
-fs.readdir(root, (err, files) => {
-  files.forEach((file) => {
-    // fs.readFile(`${root}${file}/package.json`).forEach((each) => {
-    //   console.log(each);
-    // });
-  });
-});
+const plugins = [
+  svelte({
+    preprocess: sveltePreprocess(),
+    compilerOptions: {
+      // enable run-time checks when not in production
+      dev: false,
+    },
+  }),
 
-glob("**/*.js", {}, function (err, files) {
-  // files is an array of filenames.
-  // If the `nonull` option is set, and nothing
-  // was found, then files is ["**/*.js"]
-  // er is an error object or null.
-});
+  // If you have external dependencies installed from
+  // npm, you'll most likely need these plugins. In
+  // some cases you'll need additional configuration -
+  // consult the documentation for details:
+  // https://github.com/rollup/plugins/tree/master/packages/commonjs
+  resolve({
+    browser: true,
+    dedupe: ["svelte"],
+  }),
+  commonjs(),
+  typescript({
+    sourceMap: false,
+  }),
+
+  size(),
+];
+
 export default [
   {
     input: "packages/svelte-reactive-form/src/index.ts",
     output: [
       {
+        file: "packages/svelte-reactive-form/lib/index.min.js",
+        format: "iife",
+        name: "svelteReactiveForm",
+        plugins: [terser()],
+      },
+      {
         file: "packages/svelte-reactive-form/lib/index.js",
         format: "iife",
         name: "svelteReactiveForm",
       },
-      { file: "packages/svelte-reactive-form/lib/index.cjs", format: "cjs" },
-      { file: "packages/svelte-reactive-form/lib/index.mjs", format: "es" },
+      {
+        file: "packages/svelte-reactive-form/lib/index.cjs",
+        format: "cjs",
+      },
+      {
+        file: "packages/svelte-reactive-form/lib/index.mjs",
+        format: "es",
+      },
     ],
-    plugins: [
-      svelte({
-        preprocess: sveltePreprocess(),
-        compilerOptions: {
-          // enable run-time checks when not in production
-          dev: false,
-        },
-      }),
-
-      // If you have external dependencies installed from
-      // npm, you'll most likely need these plugins. In
-      // some cases you'll need additional configuration -
-      // consult the documentation for details:
-      // https://github.com/rollup/plugins/tree/master/packages/commonjs
-      resolve({
-        browser: true,
-        dedupe: ["svelte"],
-      }),
-      commonjs(),
-      typescript({
-        sourceMap: false,
-      }),
-
-      size(),
-
-      // If we're building for production (npm run build
-      // instead of npm run dev), minify
-      // terser(),
-    ],
+    plugins,
   },
   {
     input: "packages/rules/src/index.ts",
     output: [
+      {
+        file: "packages/rules/lib/index.min.js",
+        format: "iife",
+        name: "svelteReactiveFormRules",
+        plugins: [terser()],
+      },
       {
         file: "packages/rules/lib/index.js",
         format: "iife",
@@ -75,34 +75,6 @@ export default [
       { file: "packages/rules/lib/index.cjs", format: "cjs" },
       { file: "packages/rules/lib/index.mjs", format: "es" },
     ],
-    plugins: [
-      svelte({
-        preprocess: sveltePreprocess(),
-        compilerOptions: {
-          // enable run-time checks when not in production
-          dev: false,
-        },
-      }),
-
-      // If you have external dependencies installed from
-      // npm, you'll most likely need these plugins. In
-      // some cases you'll need additional configuration -
-      // consult the documentation for details:
-      // https://github.com/rollup/plugins/tree/master/packages/commonjs
-      resolve({
-        browser: true,
-        dedupe: ["svelte"],
-      }),
-      commonjs(),
-      typescript({
-        sourceMap: false,
-      }),
-
-      size(),
-
-      // If we're building for production (npm run build
-      // instead of npm run dev), minify
-      // terser(),
-    ],
+    plugins,
   },
 ];
