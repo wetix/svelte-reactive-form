9f7d198ade021dcf070b719c75dd8c0ab45fc118
Si3nLoong <sianloong90@gmail.com>
2021-01-29 18:46:14 +0800

    test: update test script using `jest` and `testing-library`

---
+++ packages/svelte-reactive-form/src/__tests__/App.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { useForm, Field, defineRule } from "..";
+  import { required } from "../../../rules/src";
+
+  defineRule("required", required);
+
+  const form$ = useForm({
+    validateOnChange: true,
+  });
+  const { control } = form$;
+</script>
+
+<Field
+  {control}
+  name="email"
+  rules={["required"]}
+  validateOnMount={true}
+  let:onFocus
+  let:onChange
+  let:pending
+  let:dirty
+  let:valid
+  let:touched
+  let:value
+>
+  <input
+    placeholder="Email"
+    type="text"
+    {value}
+    on:focus={onFocus}
+    on:input={onChange}
+  />
+  <div data-testid="email-result">
+    {JSON.stringify({ pending, dirty, valid, value, touched })}
+  </div>
+</Field>
+
+<div data-testid="result">{JSON.stringify($form$)}</div>
+
+<style>
+  /* your styles go here */
+</style>
--- packages/svelte-reactive-form/src/__tests__/svelte-reactive-form.test.js
+++
@@ -1,5 +0,0 @@
-// const svelteReactiveForm = require("..");
-
-describe("svelte-reactive-form", () => {
-  it("needs tests", () => {});
-});
