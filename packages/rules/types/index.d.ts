433f54803d672e2f9aa84f7d7e456af7d5616788
Jeff Cheah <goodboy96@live.com.my>
2021-02-28 11:19:13 +0800

    Remain formatting

--- packages/rules/types/index.d.ts
+++ packages/rules/types/index.d.ts
@@ -1,20 +1,10 @@
-import type {
-  FormControl,
-  ValidationResult,
-} from "../../svelte-reactive-form/src/types";
+import type { FormControl, ValidationResult } from "../../svelte-reactive-form/src/types";
 export declare const required: (v: any) => ValidationResult;
 export declare const alphaNum: (v: string) => ValidationResult;
-export declare const between: (
-  v: any,
-  [min, max]: string[]
-) => ValidationResult;
+export declare const between: (v: any, [min, max]: string[]) => ValidationResult;
 export declare const url: (v: string) => ValidationResult;
 export declare const unique: <T>(v: T[]) => ValidationResult;
-export declare const same: (
-  v: any,
-  [field]: string[],
-  ctx: FormControl
-) => ValidationResult;
+export declare const same: (v: any, [field]: string[], ctx: FormControl) => ValidationResult;
 export declare const email: (v: string) => ValidationResult;
 export declare const contains: <T>(v: T, list: T[]) => ValidationResult;
 export declare const minLength: (val: any, [min]: [number]) => ValidationResult;
--- packages/svelte-reactive-form/types/types.d.ts
+++ packages/svelte-reactive-form/types/types.d.ts
@@ -2,122 +2,89 @@ import type { Readable, Writable } from "svelte/store";
 export declare type Fields = Record<string, any>;
 export declare type FieldValue = any;
 interface Resolver {
-  validate(data: any): Promise<any>;
+    validate(data: any): Promise<any>;
 }
 export declare type Config = {
-  resolver?: Resolver;
-  validateOnChange?: boolean;
+    resolver?: Resolver;
+    validateOnChange?: boolean;
 };
-export declare type SuccessCallback = (
-  data: Record<string, any>,
-  e: Event
-) => any;
-export declare type ErrorCallback = (
-  errors: Record<string, any>,
-  e: Event
-) => any;
-export declare type NodeElement =
-  | HTMLInputElement
-  | HTMLSelectElement
-  | HTMLTextAreaElement;
+export declare type SuccessCallback = (data: Record<string, any>, e: Event) => any;
+export declare type ErrorCallback = (errors: Record<string, any>, e: Event) => any;
+export declare type NodeElement = HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement;
 declare type Success = true;
 declare type Error = string;
 export declare type ValidationResult = Success | Error;
-export declare type ValidationFunction = (
-  ...args: any[]
-) => ValidationResult | Promise<ValidationResult>;
+export declare type ValidationFunction = (...args: any[]) => ValidationResult | Promise<ValidationResult>;
 export declare type Validator = string | ValidationFunction;
-export declare type RuleExpression =
-  | string
-  | Array<Validator>
-  | Record<string, Validator>
-  | Record<string, any>;
+export declare type RuleExpression = string | Array<Validator> | Record<string, Validator> | Record<string, any>;
 export declare type RegisterOption<T> = {
-  defaultValue?: T;
-  bail?: boolean;
-  validateOnBlur?: boolean;
-  validateOnMount?: boolean;
-  rules?: RuleExpression;
+    defaultValue?: T;
+    bail?: boolean;
+    validateOnBlur?: boolean;
+    validateOnMount?: boolean;
+    rules?: RuleExpression;
 };
 export declare type FieldOption = {
-  defaultValue?: any;
-  rules?: RuleExpression;
-  validateOnMount?: boolean;
-  handleChange?: (state: FieldState, node: Element) => void;
+    defaultValue?: any;
+    rules?: RuleExpression;
+    validateOnMount?: boolean;
+    handleChange?: (state: FieldState, node: Element) => void;
 };
 export interface FormControl {
-  register: <T>(
-    path: string,
-    option?: RegisterOption<T>
-  ) => Readable<FieldState>;
-  unregister: (path: string) => void;
-  setValue: (path: string, value: any) => void;
-  getValue: (path: string) => any;
-  getValues: () => Record<string, any>;
-  setError: (path: string, values: string[]) => void;
-  setTouched: (path: string, state: boolean) => void;
-  reset: (values?: Fields) => void;
+    register: <T>(path: string, option?: RegisterOption<T>) => Readable<FieldState>;
+    unregister: (path: string) => void;
+    setValue: (path: string, value: any) => void;
+    getValue: (path: string) => any;
+    getValues: () => Record<string, any>;
+    setError: (path: string, values: string[]) => void;
+    setTouched: (path: string, state: boolean) => void;
+    reset: (values?: Fields) => void;
 }
-declare interface FieldErrors extends Readable<Fields> {}
-declare type UseField = (
-  node: HTMLElement,
-  option?: FieldOption
-) => {
-  update(v: FieldOption): void;
-  destroy(): void;
+declare interface FieldErrors extends Readable<Fields> {
+}
+declare type UseField = (node: HTMLElement, option?: FieldOption) => {
+    update(v: FieldOption): void;
+    destroy(): void;
 };
 export interface Form extends Readable<FormState>, FormControl {
-  control: Readable<FormControl>;
-  field: UseField;
-  errors: FieldErrors;
-  validate: (
-    paths?: string | Array<string>
-  ) => Promise<{
-    valid: boolean;
-    data: object;
-  }>;
-  onSubmit: (
-    success: SuccessCallback,
-    error?: ErrorCallback
-  ) => (e: Event) => void;
+    control: Readable<FormControl>;
+    field: UseField;
+    errors: FieldErrors;
+    validate: (paths?: string | Array<string>) => Promise<{
+        valid: boolean;
+        data: object;
+    }>;
+    onSubmit: (success: SuccessCallback, error?: ErrorCallback) => (e: Event) => void;
 }
 export declare type FormState = {
-  pending: boolean;
-  submitting: boolean;
-  dirty: boolean;
-  touched: boolean;
-  valid: boolean;
+    pending: boolean;
+    submitting: boolean;
+    dirty: boolean;
+    touched: boolean;
+    valid: boolean;
 };
 export declare type FieldState = {
-  defaultValue: any;
-  value: any;
-  pending: boolean;
-  dirty: boolean;
-  touched: boolean;
-  valid: boolean;
-  errors: string[];
+    defaultValue: any;
+    value: any;
+    pending: boolean;
+    dirty: boolean;
+    touched: boolean;
+    valid: boolean;
+    errors: string[];
 };
 export interface FieldStateStore extends Writable<FieldState> {
-  destroy(): void;
+    destroy(): void;
 }
 export declare type ValidationRule = {
-  name: string;
-  validate: (
-    value: any,
-    params?: string[],
-    ctx?: FormControl
-  ) => Promise<ValidationResult>;
-  params: any[];
+    name: string;
+    validate: (value: any, params?: string[], ctx?: FormControl) => Promise<ValidationResult>;
+    params: any[];
 };
 export declare type ResetFormOption = {
-  errors: boolean;
-  dirtyFields: boolean;
+    errors: boolean;
+    dirtyFields: boolean;
 };
-export declare type Field = [
-  FieldStateStore,
-  ValidationRule[],
-  {
+export declare type Field = [FieldStateStore, ValidationRule[], {
     bail: boolean;
-  }
-];
+}];
 export {};
