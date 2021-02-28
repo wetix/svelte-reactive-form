983cfb3f839d2d6b7840613be8908e43ebe3e053
Jeff Cheah <goodboy96@live.com.my>
2021-02-28 11:13:22 +0800

    Use true rather than boolean

--- packages/rules/types/index.d.ts
+++ packages/rules/types/index.d.ts
@@ -1,13 +1,23 @@
-import type { FormControl } from "svelte-reactive-form/src/types";
-export declare const required: (v: any) => boolean | string;
-export declare const alphaNum: (v: string) => boolean | string;
-export declare const between: (v: any, [min, max]: string[]) => boolean | string;
-export declare const url: (v: string) => boolean | string;
-export declare const unique: <T>(v: T[]) => boolean | string;
-export declare const same: (v: any, [field]: string[], ctx: FormControl) => boolean | string;
-export declare const email: (v: string) => boolean | string;
-export declare const contains: <T>(v: T, list: T[]) => boolean | string;
-export declare const minLength: (val: any, [min]: [number]) => boolean | string;
-export declare const max: (v: any, [len]: [string]) => boolean | string;
-export declare const min: (v: any, [len]: [string]) => boolean | string;
-export declare const integer: (v: string) => boolean | string;
+import type {
+  FormControl,
+  ValidationResult,
+} from "../../svelte-reactive-form/src/types";
+export declare const required: (v: any) => ValidationResult;
+export declare const alphaNum: (v: string) => ValidationResult;
+export declare const between: (
+  v: any,
+  [min, max]: string[]
+) => ValidationResult;
+export declare const url: (v: string) => ValidationResult;
+export declare const unique: <T>(v: T[]) => ValidationResult;
+export declare const same: (
+  v: any,
+  [field]: string[],
+  ctx: FormControl
+) => ValidationResult;
+export declare const email: (v: string) => ValidationResult;
+export declare const contains: <T>(v: T, list: T[]) => ValidationResult;
+export declare const minLength: (val: any, [min]: [number]) => ValidationResult;
+export declare const max: (v: any, [len]: [string]) => ValidationResult;
+export declare const min: (v: any, [len]: [string]) => ValidationResult;
+export declare const integer: (v: string) => ValidationResult;
