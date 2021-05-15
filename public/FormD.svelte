a76c3c580f8742de3ef7e7fd62778670fa2d6bef
Si3nLoong <sianloong90@gmail.com>
2021-05-15 14:31:23 +0800

    📦️ fix: packages

--- public/FormD.svelte
+++ public/FormD.svelte
@@ -1,22 +1,39 @@
-<script>
+<script lang="ts">
   import { useForm, Field } from "../packages/svelte-reactive-form/src";
   import Input from "./Input.svelte";
 
   const form$ = useForm({
-    // validateOnChange: true,
+    validateOnChange: true,
   });
   const { control } = form$;
 
-  const items = [{ address: ["", "", ""] }];
+  const asyncPhone = (): Promise<boolean> =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("resolve phone");
+        resolve(true);
+      }, 3000);
+    });
 
   const onSubmit = () => {
     console.log(form$.getValues());
   };
 </script>
 
-<Field {control} name="phoneNo" let:errors let:valid let:onChange let:onBlur>
+<Field
+  {control}
+  name="phoneNo"
+  rules={["required", asyncPhone]}
+  let:errors
+  let:valid
+  let:pending
+  let:value
+  let:onChange
+  let:onBlur
+>
   <div>Phone No</div>
   <Input secondary={true} {onChange} {onBlur} />
+  <p>{pending}{value}</p>
   <div>{JSON.stringify({ valid, errors })}</div>
 </Field>
 <Field {control} name="password" let:errors let:valid let:onChange let:onBlur>
